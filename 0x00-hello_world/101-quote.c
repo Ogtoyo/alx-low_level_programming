b211cd92d83eeff174e6905bb9cf65db1187cd5d
Ogunfowodu Toyosi <Ogunfowodutoyosi2000@gmail.com>
2023-09-07 08:36:12 -0700

    a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) 
+{
+	char code[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n;
+
+	write(1, code, sizeof(code) - 1);
+
+	return (1);
+}
