da847681a1fba923cd600419c382c098b4719a8e
Ogunfowodu Toyosi <Ogunfowodutoyosi2000@gmail.com>
2023-09-08 11:58:12 -0700

    ALPHabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 65; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(10);
+	return (0);
+}
