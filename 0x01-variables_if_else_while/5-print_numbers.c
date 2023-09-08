714706925691e69ac96c956cf78592540b8d9fa9
Ogunfowodu Toyosi <Ogunfowodutoyosi2000@gmail.com>
2023-09-08 13:37:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+
+	return (0);
+}
