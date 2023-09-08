7456fdd6394e741ad1603f0c5aa80570a9948889
Ogunfowodu Toyosi <Ogunfowodutoyosi2000@gmail.com>
2023-09-08 13:58:00 -0700

    in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing the alphabet in lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 122; ch <= 97; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar(10);
+	return (0);
+}
