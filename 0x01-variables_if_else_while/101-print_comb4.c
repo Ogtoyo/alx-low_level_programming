14b70c0fe86c22afbb8578a3a420f5d0fe2272e0
Ogunfowodu Toyosi <Ogunfowodutoyosi2000@gmail.com>
2023-09-08 18:33:51 -0700

    do more of what you do

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - printing all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num_1, num_2, num_3;
+
+	for (num_1 = 0; num_1 < 8; num_1++)
+	{
+		for (num_2 = num_1 + 1; num_2 < 9; num_2++)
+		{
+			for (num_3 = num_1 + 2; num_3 < 10; num_3++)
+			{
+
+				putchar(num_1 + '0');
+				putchar(num_2 + '0');
+				putchar(num_3 + '0');
+
+				if (num_1 != 8 || num_2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+	}
+	putchar(10);
+
+	return (0);
+}
