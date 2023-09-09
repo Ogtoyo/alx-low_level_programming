12f6c000ee9eba76451d659394662ba46b8595bb
Ogunfowodu Toyosi <Ogunfowodutoyosi2000@gmail.com>
2023-09-08 19:15:57 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num_1 = 0; num_1 < 100; num_1++)
 	{
-		for (num_2 = num_1; num_2 < 100; num_2++)
+		for (num_2 = num_1 + 1; num_2 < 100; num_2++)
 		{
 
 			putchar(num_1 / 10 + '0');
@@ -21,7 +21,7 @@ int main(void)
 			putchar(' ');
 
 			putchar(num_2 / 10 + '0');
-			putchar(num_2 % 10 + '1');
+			putchar(num_2 % 10 + '0');
 
 			if (num_1 != 98 || num_2 != 99)
 			{
