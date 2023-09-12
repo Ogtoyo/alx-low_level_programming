3af4da035347e4cfaaafd2597363e5be93ac51a4
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-12 21:43:51 +0100

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,8 @@ void times_table(void)
 			if (y == 0)
 			{
 				putchar(z + '0');
-			} else if (z < 10 && y != 0)
+			}
+			if (z < 10 && y != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -30,7 +31,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(z + '0');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
 			}
 		}
 
