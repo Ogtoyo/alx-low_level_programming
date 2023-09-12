5346ebf03c23f0b097e1e11c1c51408e67bb5b33
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-12 23:50:29 +0100

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ *
+ * @x: first character
+ *
+ * @y: second charater
+ *
+ * Return: Always 0
+ */
+
+int add(int x, int y)
+{
+
+	sum = x + y;
+
+	return (sum);
+}
