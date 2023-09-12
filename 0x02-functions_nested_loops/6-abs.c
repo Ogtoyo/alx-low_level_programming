d76a9b2acec02601d7cf6bc59e86677296d8197f
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-12 19:20:23 +0100

    You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 
 /**
- * _abs - a function that computes the absolute value of an integer.
+ * _abs - a function that computes the absolute value of an integer
  *
  *@i: to print absolute
+ *Return: absolute value
  */
 
 int _abs(int i)
