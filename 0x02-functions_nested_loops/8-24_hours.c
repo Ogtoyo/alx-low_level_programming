92cc52e4b323e6ac749b523d6cde96dd70e46468
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-12 20:43:03 +0100

    today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - a function that prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0 (Success).
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hr, int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60)
+		{
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar(10);
+
+			return (0);
+
+		}
+	}
+}
+
