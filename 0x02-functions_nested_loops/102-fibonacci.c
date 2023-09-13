9940bee0a78555bd00829f4015d4975321a7ff2c
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-13 09:52:02 +0100

    I wrote a program that would steal passwords of students

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,13 +15,13 @@ int main(void)
 	num1 = 1;
 	num2 = 2;
 
-	printf("%d, %d, ", num1, num2);
+	printf("%ld, %ld, ", num1, num2);
 
 	for (count = 3; count <= 50; count++)
 	{
 		next = num1 + num2;
 
-		printf("%d", next);
+		printf("%ld", next);
 
 		if (count < 50)
 			printf(", ");
