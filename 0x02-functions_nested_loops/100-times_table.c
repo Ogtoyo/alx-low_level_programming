69077af629e35f57f74fcc40d1cef5b8a0e3b35b
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-13 02:01:54 +0100

    ntimes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(z + '0');
-				} else if (z >= 10)
+				} else if (z >= 10 && z < 100)
 				{
 					_putchar(' ');
 					_putchar(z / 10 + '0');
