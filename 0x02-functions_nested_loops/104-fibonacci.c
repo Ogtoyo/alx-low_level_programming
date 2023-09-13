2d922ee310dc2929ddb521e1135bc0fd3642e82a
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-13 23:33:57 +0100

    the first assignment was to write a program to print the first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,27 +9,37 @@
 
 int main(void)
 {
-	unsigned long long int num1, num2, next;
-	int count;
+	unsigned long long int num1, num2, n, num1A, num2A, num1B, num2B, count;
 
 	num1 = 1;
 	num2 = 2;
+	n = 1000000000;
 
-	printf("%llu, %llu, ", num1, num2);
+	printf("%lu", num1);
 
-	for (count = 3; count <= 98; count++)
+	for (n = 1; n < 91; n++)
 	{
-		next = num1 + num2;
+		printf(", %lu", num2);
 
-		printf("%llu", next);
-
-		if (count < 98)
-			printf(", ");
-
-		num1 = num2;
-		num2 = next;
-
+		num2 += num1;
+		num1 = num2 - num1;
 	}
+		num1A = num1 / n;
+		num1B = num1 % n;
+		num2A = num2 / n;
+		num2B = num2 % n;
+
+	for (n = 92; n < 99; n++)
+	{
+		printf(", %lu", num2A + (num2B / n));
+		printf("%09lu", num2B % n);
+
+		num2A = num2A + num1A + (num2B / n);
+		num1A = num2A - num1A;
+		num2B = num2B + num1B;
+		num1B = num2B - num1B;
+	}
+
 
 	printf("\n");
 
