a7cec2933151459da436afed9b7142cd740844e1
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-14 16:12:07 +0100

    multiply life

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two integers
+ *
+ * @a: first character
+ *
+ * @b: second charater
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
