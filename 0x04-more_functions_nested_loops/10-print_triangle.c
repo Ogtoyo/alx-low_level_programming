efcacd2705e0363c91bc866181e700cb8ff8bc98
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-15 00:45:07 +0100

    triangle things

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_triangle - a function that prints a triangle
+ *@size: size of tri
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i -1; j++)
+			{
+				putchar(' ');
+			}
+
+			for (j = 0; j <= i; j++)
+			{
+				putchar('#');
+			}
+
+			putchar(10);
+
+		}
+
+	} else
+		putchar(10);
+}
