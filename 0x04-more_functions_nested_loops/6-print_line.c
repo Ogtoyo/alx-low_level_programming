784b5a86dcd22ddd7687274dd1e4d4497340b936
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-14 17:34:30 +0100

    printing line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_line - a function that draws a straight line in the terminal
+ *@n: print char
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		putchar(10);
+
+	else
+		putchar("_");
+}
