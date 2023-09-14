6a7a37e7f83e413bb7e346178170b3888228a1d9
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-14 16:07:43 +0100

    it is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,12 @@
  *
  * @c: character check
  *
- * Return: 1 if c is a letter, lowercase or uppercase or 0 otherwise
+ * Return: 1 if c is a digit 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c < 10)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
