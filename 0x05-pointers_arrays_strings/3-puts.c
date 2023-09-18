5707a1c56b06b50dbcd780d0f6a6a310d568f39a
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-18 17:09:37 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts - a function that prints a string, followed by a new line
+ *
+ *@str: pointer charater
+ *Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		_putchar(*str);
+	}
+
+	putchar('\n');
+}
