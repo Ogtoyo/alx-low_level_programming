562873cf0eb744e99e89b61c0fd740d246c5c11c
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-19 03:49:39 +0100

    Half the lies they tell about me

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,12 +14,14 @@ void puts2(char *str)
 
 	i = 0;
 
-	while (str[i] != '\0' && str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[i] == '\0')
-			break;
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
 	}
 
 	_putchar('\n');
