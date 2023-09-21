ec48ccd628f78a2d77a6544fea06c2d58323f279
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-22 00:14:56 +0100

    Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *print_number - a function that prints an integer
+ *@n: intergers
+ *Return: Always return 0.
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+	int num, path;
+
+	i = (n < 0) ? -n : n;
+
+	if (n < 0)
+		_putchar('-');
+
+	num = 1;
+	path = i;
+
+	while (path > 9)
+	{
+		path /= 10;
+		num *= 10;
+	}
+
+	while (num > 0)
+	{
+		_putchar((i / num) + '0');
+		i %= num;
+		num /= 10;
+	}
+}
