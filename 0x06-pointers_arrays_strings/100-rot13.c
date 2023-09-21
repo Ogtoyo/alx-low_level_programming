ccaaf1c02693ad1ff31e066e399f238140dab111
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-21 20:05:45 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *rot13 - a function that encodes a string using rot13
+ *@s: string
+ *Return: modifing the string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char s_1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s_2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; s_1[j]; j++)
+		{
+			if (s[i] == s_1[j])
+			{
+				s[i] = s_2[j];
+			}
+		}
+	}
+
+	return (s);
+}
