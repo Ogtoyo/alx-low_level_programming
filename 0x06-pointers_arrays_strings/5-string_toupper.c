19f4eab29027dd5a21fba4d9047ba6e81bfdebaa
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-21 15:21:45 +0100

    converting to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - a function that reverses the content of an array of integers
+ *s: strinfg
+ *Return: modifing the string
+ */
+
+char *string_toupper(char *s)
+{
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = s[i] -32;
+        }
+    }
+
+    return (s);
+}
