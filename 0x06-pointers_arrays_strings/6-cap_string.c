a80ac95beb37849ee0b0770974351ae5a28c9f27
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-21 16:40:43 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ *cap_string - a function that capitalizes all words of a string
+ *@s: string
+ *Return: modifing the string
+ */
+
+char *cap_string(char *s)
+{
+    int i;
+
+    if (strlen(s) > 0 && islower(s[0]))
+    {
+        s[0] = toupper(s[0]);
+    }
+
+    for (i = 1; s[i] != '\0'; i++)
+    {
+        if (s[i] == ' ' ||
+        s[i - 1] == '\t' ||
+        s[i - 1] == '\n' ||
+        s[i - 1] == ',' ||
+        s[i - 1] == ';' || 
+        s[i - 1] == '.' || 
+        s[i - 1] == '!'|| 
+        s[i - 1] == '?' || 
+        s[i - 1] == '"' || 
+        s[i - 1] == '(' || 
+        s[i - 1] == ')' || 
+        s[i - 1] == '{' || 
+        s[i - 1] == '}')
+        {
+            if (islower(s[i])) 
+            {
+                s[i] = toupper(s[i]);
+            }
+        }
+
+    }
+
+    return (s);
+}
