bad90701570a4683da00ac3ffc8e2072c7d392d4
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-21 19:24:29 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,37 @@
  *Return: modifing the string
  */
 
-char *cap_string(char *s) {
-    int i;
+char *cap_string(char *s)
+{
+	int i;
 
-    if (strlen(s) > 0 && islower(s[0])) {
-        s[0] = toupper(s[0]);
-    }
+	if (strlen(s) > 0 && islower(s[0]))
+	{
+		s[0] = toupper(s[0]);
+	}
 
-    for (i = 1; s[i] != '\0'; i++) {
-        if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
-            s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-            s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-            s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-            s[i - 1] == '}')
-        {
-            if (islower(s[i])) {
-                s[i] = toupper(s[i]);
-            }
-        }
-    }
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if (s[i - 1] == ' ' ||
+		s[i - 1] == '\t' ||
+		s[i - 1] == '\n' ||
+		s[i - 1] == ',' ||
+		s[i - 1] == ';' ||
+		s[i - 1] == '.' ||
+		s[i - 1] == '!' ||
+		s[i - 1] == '?' ||
+		s[i - 1] == '"' ||
+		s[i - 1] == '(' ||
+		s[i - 1] == ')' ||
+		s[i - 1] == '{' ||
+		s[i - 1] == '}')
+		{
+			if (islower(s[i]))
+			{
+				s[i] = toupper(s[i]);
+			}
+		}
+	}
 
-    return s;
+	return (s);
 }
