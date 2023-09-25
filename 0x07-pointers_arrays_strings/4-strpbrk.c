6e3351d2ad33f80eb4ced550920c62c516bf82a5
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-25 17:00:00 +0100

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,19 +12,19 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
 
-            if (s[i] == accept[j])
-            {
-                return (&s[i]);
-            }
-        }
-    }
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
