6d9150691046e81fbca2734a07321cf273f611e3
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-25 17:52:49 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,20 +12,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0' && needle[j] == haystack[i + j]; j++)
-        {
-            ;
-        }
-        if (needle[j] == '\0')
-        {
-            return (&haystack[i]);
-        }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0' && needle[j] == haystack[i + j]; j++)
+		{
+			;
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
 
-    }
+	}
 
-    return (NULL);
+	return (NULL);
 }
