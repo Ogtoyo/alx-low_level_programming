776a58374713e4d98da01a4dc92b9714ebbdc9ec
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-25 16:31:13 +0100

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,28 +12,28 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int i, j, found;
-    unsigned int count = 0;
+	int i, j, found;
+	unsigned int count = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        found = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
 
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (accept[j] == s[i])
-            {
-                count++;
-                found = 1;
-                break;
-            }
-        }
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				count++;
+				found = 1;
+				break;
+			}
+		}
 
-        if (!found)
-        {
-            break;
-        }
-    }
+		if (!found)
+		{
+			break;
+		}
+	}
 
-    return (i);
+	return (i);
 }
