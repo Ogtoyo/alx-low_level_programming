33a6ee2b30a10f749e8047454b01ef8e7f155c1f
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-25 22:53:48 +0100

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *set_string - a function that prints sum of two diagonals of matrix
+ *
+ * @s: pointer to a pointer to a character
+ * @to: pointer to character
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
