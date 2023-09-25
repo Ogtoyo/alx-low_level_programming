59d4551f131c0d97c86093af5ca265af481282df
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-25 15:29:41 +0100

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,14 +13,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i <= n - 1; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i <= n - 1; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    return (dest);
+	return (dest);
 
 }
 
