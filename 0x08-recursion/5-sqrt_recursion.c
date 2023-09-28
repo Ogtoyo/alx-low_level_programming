bd84e8a07a16a449d20863c3f6eb9139dfc0937e
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-28 15:16:15 +0100

    squ root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number.
+ *
+ *@n: pointer of string
+ *Return: Always 0
+ */
+
+int squ_finder(int n, int num)
+{
+    if (num * num == n)
+        return (num);
+
+    else if (num * num < n)
+    {
+        return (squ_finder(n, num + 1));
+    }
+
+    else
+    {
+        return (-1);
+    }
+}
+
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+
+    return (squ_finder(n, 0));
+}
