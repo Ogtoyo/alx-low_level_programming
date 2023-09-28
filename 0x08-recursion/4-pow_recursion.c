d9e871ef41ee92bfb1fda0342353fe532609aa2f
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-09-28 14:05:10 +0100

    powers

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *_pow_recursion -  a function that returns the value of x raised to the power of y.
+ *_pow_recursion - returns the value of x raised to the power of y
  *
  *@x: return value
  *@y: power
