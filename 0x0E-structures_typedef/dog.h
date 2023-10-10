9c12194d0728cd111c68e04b95adb3ae216d75a4
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-10 12:55:55 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - Dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+*/
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif
