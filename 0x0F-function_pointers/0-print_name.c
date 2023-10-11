d5e4f7fd30b9c1a84b19e19a2eae6a189e578c1a
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-11 09:50:29 +0100

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: a pointer to a string representing the name
+ * @f: function pointer that takes a pointer to a string as an argument
+ *
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+pointers to functions on alx
