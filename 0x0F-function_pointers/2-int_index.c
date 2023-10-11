d289d57185ecea041630af47d5d580f9811789d0
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-11 16:03:22 +0100

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index -  a function that searches for an integer
+ * @array: a pointer to an integer array
+ * @size: the size of the array
+ * @cmp: function that compares integers and returns an integer
+ *Return: Always 0
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+
+		if (result != 0)
+			return (i);
+	}
+
+	return (-1);
+}
