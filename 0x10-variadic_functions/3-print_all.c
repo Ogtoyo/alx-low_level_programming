3d39392a6c2f5a6ef47b3c86ecf1ccbdd71ed730
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-13 01:27:07 +0100

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,9 +33,8 @@ void print_all(const char * const format, ...)
 				case 's':
 					str = va_arg(args, char *);
 					if (str == NULL)
-						printf("(nil)");
-					else
-						printf("%s", str);
+						str = "(nil)";
+					printf("%s", str);
 					break;
 				default:
 					i++;
