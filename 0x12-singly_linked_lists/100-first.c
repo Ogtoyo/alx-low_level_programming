6dfff7dd2e2a175953a0783d6fbf7897b5b5f65b
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-19 22:19:58 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * before_main - Prints a message before the main function is executed.
+ *
+ * This function is called before the main function and prints a message.
+ * It is an initialization or setup routine before the main program logic
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
