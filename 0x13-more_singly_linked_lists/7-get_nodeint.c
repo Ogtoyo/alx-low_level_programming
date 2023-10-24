0876c2a7ead6a8855b05e8ee985e616cc8c02c86
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-24 21:23:10 +0100

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - Retrieves a node from a listint_t list at a given index.
+ * get_nodeint_at_index - Retrieves a node from listint_t list at a given index
  * @head: A pointer to the head of the list.
  * @index: The index of the node to retrieve.
  *
  * Return: A pointer to the node at the specified index, or NULL if not found.
  *
  * Description: This function traverses a listint_t list to retrieve the node
- * at the specified index. If the index is out of range, or the list is too short,
+ * at the specified index If the index is out of range or the list is too short
  * it returns NULL.
  */
 
