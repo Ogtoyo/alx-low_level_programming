e4a3cbb89f23d01767b326858e3a6670b0d0a4f6
Ogtoyo <Ogunfowodutoyosi2000@gmail.com>
2023-10-25 03:18:18 +0100

    Find the loop

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a linked list safely.
- * @h: A pointer to the first node in the linked list.
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: A pointer to a pointer to the head of the listint_t list.
  *
- * Return: The number of elements in the freed list.
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t length = 0;
-	int difference;
+	size_t len = 0;
 	listint_t *temp;
 
 	if (!h || !*h)
@@ -17,24 +16,13 @@ size_t free_listint_safe(listint_t **h)
 
 	while (*h)
 	{
-		difference = *h - (*h)->next;
-		if (difference > 0)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			length++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			length++;
-			break;
-		}
+		temp = (*h)->next;
+		free(*h);
+		*h = temp;
+		len++;
 	}
 
 	*h = NULL;
 
-	return (length);
+	return (len);
 }
